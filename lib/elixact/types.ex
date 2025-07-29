86cbe325bdc9af37db633a1f3911c6794549fee4
Lisu11 <Piotr_Lisowski@external.mckinsey.com>
2025-07-29 17:59:23 +0200

    chore: typo

--- lib/elixact/types.ex
+++ lib/elixact/types.ex
@@ -99,7 +99,7 @@ defmodule Elixact.Types do
 
       other ->
         if match?({:module, _}, Code.ensure_compiled(other)) &&
-+             function_exported?(other, :type_definition, 0) do
+             function_exported?(other, :type_definition, 0) do
           other.type_definition()
         else
           # Assume schema module reference
